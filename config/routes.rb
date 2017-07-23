dccfec277dd875cdf2d9457259194dcfa379e1cd
SomwangShep <ss1792@mcla.edu>
2017-07-23 22:47:02 +0000

    Inital commit

---
+++ config/routes.rb
@@ -0,0 +1,28 @@
+Rails.application.routes.draw do
+  root "pages#home"
+  get 'pages/home', to: 'pages#home'
+  
+  #resources :recipes
+  #get '/recipes', to: 'recipes#index'
+  #get '/recipes/new', to: 'recipes#new', as: 'new_recipe' 
+  #get '/recipes/:id', to: 'recipes#show', as: 'recipe'
+  
+  resources :recipes do
+    resources :comments, only: [:create]
+  end
+  
+  get '/signup', to: 'chefs#new'
+  resources :chefs, except: [:new]
+  
+  get '/login', to: 'sessions#new'
+  post '/login', to: "sessions#create"
+  delete '/logout', to: "sessions#destroy"  
+  
+  resources :ingredients, except: [:destroy]
+  
+  mount ActionCable.server => '/cable'
+  get '/chat', to: 'chatrooms#show'
+  
+  resources :messages, only: [:create]
+  
+end
